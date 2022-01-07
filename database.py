f67e5551b8abb70a67505d7187c48414a8055c9c
Leonardo <leonardo.aa88@gmail.com>
2022-01-07 03:00:48 -0300

    Updated authentication

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import datetime
 import getpass
 import os
+from utils import *
+import bcrypt
 
 from playhouse.sqlcipher_ext import *
 
@@ -9,14 +11,67 @@ class Password(Model):
     name = TextField()
     password = TextField()
     timestamp = DateTimeField(default=datetime.datetime.now)
+    updated = DateTimeField(null=True)
 
     class Meta:
         if not os.path.isfile('./passwords.db'):
-            password = getpass.getpass('Please create the database password: ')
+            while True:
+                while True:
+                    password_input0 = getpass.getpass(
+                        "Please create a password: ")
+                    password_input = getpass.getpass(
+                        "Please confirm your password: ")
+                    if password_input0 == password_input:
+                        break
+                    else:
+                        print("The passwords don't match.")
+
+                input_entropy = entropy(list(password_input), 10)
+                input_ideal_entropy = entropy_ideal(len(password_input), 10)
+                entropy_ratio = 100*input_entropy/input_ideal_entropy
+
+                if len(password_input) < 8:
+                    print("Error: your password must be at least 8 characters long.")
+                elif any(i in SPECIAL_CHARS for i in password_input) == False:
+                    print(
+                        "Error: your password must contain at least one special character.")
+                elif any(i in ONLY_LOWERCASE for i in password_input) == False:
+                    print(
+                        "Error: your password must contain at least one lower-case character.")
+                elif any(i in ONLY_UPPERCASE for i in password_input) == False:
+                    print(
+                        "Error: your password must contain at least one upper-case character.")
+                elif entropy_ratio < 90:
+                    print("Error: your password is too simple.")
+                else:
+                    break
+
+            # Encode password
+            password = str.encode(password_input)
+            # Hash a password for the first time, with a randomly-generated salt
+            hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+            f = open("password", "w")
+            # digest = hashlib.sha256(hashed).hexdigest()
+            f.write(hashed.decode())
+            f.close()
+            is_auth = True
+            db = SqlCipherDatabase('passwords.db', passphrase=password_input)
+            database = db
         else:
-            password = getpass.getpass('Enter the database password: ')
-        db = SqlCipherDatabase('passwords.db', passphrase=password)
-        database = db
+            password_input = getpass.getpass('Enter the database password: ')
+            password = str.encode(password_input)
+            f = open("password")
+            hashed = str.encode(f.readline())
+            # Check that an unhashed password matches one that has previously been hashed
+            if bcrypt.checkpw(password, hashed):
+                print("The password entered is correct.")
+                db = SqlCipherDatabase(
+                    'passwords.db', passphrase=password_input)
+                database = db
+                is_auth = True
+            else:
+                print("The password is incorrect.")
+                is_auth = False
 
 
 def create_password():
@@ -28,7 +83,7 @@ def create_password():
         Password.create(name=input_name, password=input_password)
     except:
         print("An error occurred.")
-    finally:
+    else:
         print("Password record created successfully!")
 
 
@@ -37,7 +92,7 @@ def print_passwords():
         print("Registered passwords:\n")
         for password in Password.select():
             print(
-                f"Name: {password.name} | Password: {password.password} | Created: {password.timestamp}")
+                f"Name: {password.name} | Password: {password.password} | Created: {password.timestamp} | Updated: {password.updated}")
     except:
         print("An error occurred.")
 
@@ -50,10 +105,11 @@ def update_password():
         print("Input the new password:")
         new_password = input()
         password.password = new_password
+        password.updated = datetime.datetime.now()
         password.save()
     except:
         print("An error occurred.")
-    finally:
+    else:
         print("Password updated successfully!")
 
 
@@ -65,5 +121,5 @@ def delete_password():
         password.delete_instance()
     except:
         print("An error occurred.")
-    finally:
+    else:
         print("Record deleted successfully!")
--- password-manager.py
+++ password-manager.py
@@ -1,63 +1,15 @@
 import os
-import bcrypt
 from utils import *
 from playhouse.sqlcipher_ext import *
 from database import *
 
 if __name__ == "__main__":
 
-    is_auth = False
-
-    if not os.path.isfile('./password'):
-        while True:
-            password_input = getpass.getpass("Please create a password: ")
-
-            input_entropy = entropy(list(password_input), 10)
-            input_ideal_entropy = entropy_ideal(len(password_input), 10)
-            entropy_ratio = 100*input_entropy/input_ideal_entropy
-
-            if len(password_input) < 8:
-                print("Error: your password must be at least 8 characters long.")
-            elif any(i in SPECIAL_CHARS for i in password_input) == False:
-                print(
-                    "Error: your password must contain at least one special character.")
-            elif any(i in ONLY_LOWERCASE for i in password_input) == False:
-                print(
-                    "Error: your password must contain at least one lower-case character.")
-            elif any(i in ONLY_UPPERCASE for i in password_input) == False:
-                print(
-                    "Error: your password must contain at least one upper-case character.")
-            elif entropy_ratio < 90:
-                print("Error: your password is too simple.")
-            else:
-                break
-
+    if not os.path.isfile('./passwords.db'):
         # Create a password database for the first time
-        db = SqlCipherDatabase('passwords.db', passphrase=password_input)
         Password.create_table()
-        # Encode password
-        password = str.encode(password_input)
-        # Hash a password for the first time, with a randomly-generated salt
-        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-        f = open("password", "w")
-        # digest = hashlib.sha256(hashed).hexdigest()
-        f.write(hashed.decode())
-        f.close()
-        is_auth = True
-    else:
-        password_input = getpass.getpass("Please input your password: ")
-        password = str.encode(password_input)
-        f = open("password")
-        hashed = str.encode(f.readline())
-        # Check that an unhashed password matches one that has previously been hashed
-        if bcrypt.checkpw(password, hashed):
-            print("The password entered is correct.")
-            is_auth = True
-        else:
-            print("The password is incorrect.")
-            is_auth = False
 
-    if is_auth:
+    if Password._meta.is_auth:
         print("You are now authenticated. Welcome to password manager!")
         while(True):
             print("\n--------------------------------")
